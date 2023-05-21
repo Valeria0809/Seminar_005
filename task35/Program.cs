1498d9e8856287effec5b04ac3a6de7fdd7e6852
VALERIA <valeri.tereshenkova@icloud.com>
2023-05-21 12:16:54 +0300

    Task 35

---
+++ task35/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 35: Задайте одномерный массив из 30 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        tempArray[i] = rand.Next(leftRange, rightRange + 1);
+    }
+
+    return tempArray;
+}
+
+int CountElem(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] >= 10 && array[i] < 100)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+int[] arr = FillArray(10, 90, 99);
+PrintArray(arr);
+System.Console.WriteLine($"Количество чисел принадлежащих указанному диапазону: {CountElem(arr)}");
